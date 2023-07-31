a71853031960cc6e36346d9f8ebb34c27b912dc8
Hanchenyu <2935203537@qq.com>
2023-07-31 16:29:14 +0800

    third commit

--- test_7_20/test_7_20/test.c
+++ test_7_20/test_7_20/test.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("hehe\n");
+	int i = 0;
+	for (i = 32; i <= 127; i++)
+	{
+		printf("%c", i);
+		if (i % 16 == 15)
+		{
+			printf("\n");
+		}
+	}
 	return 0;
 }
